3a1c8de6ce9c21a6f54d25a8ae40ec83f87822fe
dheena <dheenadhayalanr19@gmail.com>
2022-05-18 12:22:58 +0530

    Correct syntax error

--- URDF_Exporter/utils/utils.py
+++ URDF_Exporter/utils/utils.py
@@ -156,12 +156,12 @@ def create_package(package_name, save_dir, package_dir):
 
     try: os.mkdir(save_dir + '/' +package_name)
     except: pass
-    with open(os.path.join(save_dir, package_name, '__init__.py'), w):
+    with open(os.path.join(save_dir, package_name, '__init__.py'), 'w'):
         pass
 
     try: os.mkdir(save_dir + '/resource')
     except: pass
-    with open(os.path.join(save_dir, 'resource', package_name), w):
+    with open(os.path.join(save_dir, 'resource', package_name), 'w'):
         pass
 
     try: os.mkdir(save_dir + '/test')
